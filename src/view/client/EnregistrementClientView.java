5e95200c4c74d958d45ee12662a31ed8b0787ba0
koolkoth <richard.abougou@gmail.com>
2025-06-05 06:13:47 +0200

    Dernières modifications clients

--- src/view/client/EnregistrementClientView.java
+++ src/view/client/EnregistrementClientView.java
@@ -1,42 +1,4 @@
 package view.client;
 
-import db.dao.ClientDAO;
-import db.dao.MenuDAO;
-import db.dao.ReservationDAO;
-import db.model.*;
-import db.model.Reservation;
-import view.Page;
-import view.View;
-
-import java.lang.invoke.Invokers$Holder;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-public class EnregistrementClientView extends View {
-
-    protected static ClientDAO clientDAO = ClientDAO.getInstance();
-    protected static Restaurent restaurent = Restaurent.getInstance();
-
-
-    public ReserverTableView() {
-        super();
-        this.actions = new String[]{
-            "Retour à votre page Client"
-        };
-
-        this.commandes.put("REGISTER", "S'enregistrer");
-    }
-
-    @Override
-    protected void content() {}
-
-    @Override
-    protected final void userAction() {
-        System.out.println("Identification");
-        String nom = this.input("Nom :");
-        String prenom = this.input("Prénom : ");
-
-        Client c = clientDAO.findClientByFullname()
-    }
+public class EnregistrementClientView {
 }
